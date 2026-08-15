cce58a1e7b9e2533db34d028167a41b930d5a3ea
Odilen Gonzalez <odilen.88@gmail.com>
2026-08-15 15:38:35 -0300

    configurar passport en la aplicacion

--- src/app.js
+++ src/app.js
@@ -1,12 +1,18 @@
 import express from 'express'
+import cookieParser from 'cookie-parser'
+import passport from 'passport'
+
 import eventsRouter from './routes/events.routes.js'
 import sessionsRouter from './routes/sessions.routes.js'
-import cookieParser from 'cookie-parser'
+import { initializePassport } from './config/passport.config.js'
 
 const app = express()
 
+initializePassport()
+
 app.use(express.json())
 app.use(cookieParser())
+app.use(passport.initialize())
 
 app.get('/api/health', (req, res) => {
   return res.status(200).json({
---
+++ src/config/passport.config.js
@@ -0,0 +1,7 @@
+import passport from 'passport'
+
+export const initializePassport = () => {
+  // Aquí agregaremos las estrategias register, login y current
+}
+
+export default passport
